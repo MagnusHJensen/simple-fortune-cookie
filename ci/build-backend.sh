626401547c7a89f0bee81a7e466d6f2a0347a027
Magnus Jensen <magnushjensen.mail@gmail.com>
2023-08-17 13:31:17 +0000

    use full commit for docker tagging

--- ci/build-backend.sh
+++ ci/build-backend.sh
@@ -1,4 +1,4 @@
 docker_username_lower=$(echo "$DOCKER_USERNAME" | tr '[:upper:]' '[:lower:]')
-[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
+[[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT}"
 DockerRepo="ghcr.io/${docker_username_lower}/"
 docker build -t "${DockerRepo}backend:latest" -t "${DockerRepo}backend:$Tag" backend/
