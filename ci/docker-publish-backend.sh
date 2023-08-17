c58c1c47f28e09c6c96049249f45ddf9007f547b
Nikolaj T <Nikolaj@email.com>
2023-08-17 10:01:49 +0000

    feature/publish-scripts

---
+++ ci/docker-publish-backend.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+docker_username_lower=$(echo "$DOCKER_USERNAME" | tr '[:upper:]' '[:lower:]')
+echo "$DOCKER_PASS" | docker login ghcr.io --username "$docker_username_lower" --password-stdin
+docker push "ghcr.io/$docker_username_lower/backend:${GIT_COMMIT::8}"
+docker push "ghcr.io/$docker_username_lower/backend:latest" &
+wait
