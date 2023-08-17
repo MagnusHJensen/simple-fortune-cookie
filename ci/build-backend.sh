a656af3f62eee49dd31426aac50545bfa73fb016
Nikolaj T <Nikolaj@email.com>
2023-08-17 10:19:46 +0000

    username-fix

--- ci/build-backend.sh
+++ ci/build-backend.sh
@@ -1,4 +1,4 @@
 docker_username_lower=$(echo "$DOCKER_USERNAME" | tr '[:upper:]' '[:lower:]')
 [[ -z "${GIT_COMMIT}" ]] && Tag='local' || Tag="${GIT_COMMIT::8}"
-[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="ghcr.io/${docker_username_lower}/"
+DockerRepo="ghcr.io/${docker_username_lower}/"
 docker build -t "${DockerRepo}backend:latest" -t "${DockerRepo}backend:$Tag" backend/
